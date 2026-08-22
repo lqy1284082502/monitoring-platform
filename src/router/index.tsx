6fb43efe6bc82705d68cdda18a82bf2211df098a
liqiyuan <liqiyuan@users.noreply.github.com>
2026-08-22 21:02:34 +0800

    Support deployment under base path

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { routeConfiguration } from './routeConfiguration.tsx';
 
-const router = createBrowserRouter(routeConfiguration);
+const router = createBrowserRouter(routeConfiguration, {
+    basename: import.meta.env.BASE_URL,
+});
 export { router };
